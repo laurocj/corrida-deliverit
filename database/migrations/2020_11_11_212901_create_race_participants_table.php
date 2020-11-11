5bd0661ebfe29abb3bb8872423cf193ee8c639c3
laurocj <laurocj@gmail.com>
2020-11-11 18:52:17 -0300

    created the migrations

---
+++ database/migrations/2020_11_11_212901_create_race_participants_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRaceParticipantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('race_participants', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('participant_id');
+            $table->unsignedBigInteger('racing_id');
+            $table->timestamps();
+
+            $table->foreign('participant_id')->references('id')->on('participants');
+            $table->foreign('racing_id')->references('id')->on('racings');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('race_participants');
+    }
+}
---
+++ database/migrations/2020_11_11_213141_create_results_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateResultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('results', function (Blueprint $table) {
+            $table->id();
+            $table->time('start');
+            $table->time('end');
+
+            $table->unsignedBigInteger('participant_id');
+            $table->unsignedBigInteger('racing_id');
+            $table->timestamps();
+
+            $table->foreign('participant_id')->references('id')->on('participants');
+            $table->foreign('racing_id')->references('id')->on('racings');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('results');
+    }
+}
