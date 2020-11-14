962930a95f6efa2c531abd3183e703d80c008137
laurocj <laurocj@gmail.com>
2020-11-14 11:22:30 -0300

    feat(Result): classification

--- app/Http/Controllers/ResultsController.php
+++ app/Http/Controllers/ResultsController.php
@@ -3,11 +3,33 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ResultsStoreRequest;
+use App\Racing;
 use App\Result;
 use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 
 class ResultsController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @param  \Illuminate\Http\Request
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $results = (new Result)->ranking();
+
+        if ($request->age) {
+            $results = $this->classificationByAge($results);
+        } else {
+            $results = $this->classificationAll($results);
+        }
+
+        return $results;
+    }
+
     /**
      * Store a newly created resource in storage.
      *
@@ -16,7 +38,7 @@ class ResultsController extends Controller
      */
     public function store(ResultsStoreRequest $request)
     {
-        if(!empty($request->finish)) {
+        if (!empty($request->finish)) {
             $startTime = Carbon::parse($request->start);
             $finishTime = Carbon::parse($request->finish);
             $request->duration = $finishTime->diff($startTime)->format('%H:%I:%S');
@@ -40,5 +62,85 @@ class ResultsController extends Controller
             'start' => $post->start,
             'finish' => $post->finish
         ], 201);
+    }
+
+    /**
+     * Returns the overall rating
+     *
+     * @param  \Illuminate\Support\Collection
+     * @return array
+     */
+    private function classificationAll(Collection $results)
+    {
+        return $this->classification($results, ['all' => []]);
+    }
+
+    /**
+     * Returns the classification by age
+     *
+     * @param  \Illuminate\Support\Collection
+     * @return array
+     */
+    private function classificationByAge(Collection $results)
+    {
+
+        $ranges = $this->classification(
+            $results,
+            [
+                '24' => [],
+                '34' => [],
+                '44' => [],
+                '54' => [],
+                '+55' => []
+            ]
+        );
+
+        foreach ([
+            '24' => '18-25',
+            '34' => '25-35',
+            '44' => '35-45',
+            '54' => '45-54',
+            '+55' => '55+'
+        ] as $key => $range) {
+
+            $ranges[$range] = $ranges[$key];
+            unset($ranges[$key]);
+        }
+
+        return $ranges;
+    }
+
+    /**
+     * Returns the sort
+     *
+     * @param  \Illuminate\Support\Collection
+     * @param  array
+     * @return array
+     */
+    private function classification(Collection $results, array $ranges)
+    {
+        $results->map(function ($result) use (&$ranges) {
+
+            $result->age = Carbon::parse($result->birth)->age;
+
+            foreach ($ranges as $breakpoint => &$array) {
+                if (is_string($breakpoint) || $breakpoint >= $result->age) {
+                    $distance = Racing::DISTANCES[$result->distance];
+
+                    $array['racing_' . $distance][] = [
+                        'racing'        => $result->racing_id,
+                        'distance'      => $distance . ' Km',
+                        'paticipant'    => $result->participant_id,
+                        'age'           => $result->age,
+                        'name'          => $result->name,
+                        'position'      => isset($array[$distance]) ? count($array[$distance]) + 1 : 1
+                    ];
+                    break;
+                }
+            }
+            return $result;
+        });
+
+        return $ranges;
     }
 }
--- app/Result.php
+++ app/Result.php
@@ -13,4 +13,14 @@ class Result extends Model
         'finish',
         'duration'
     ];
+
+    public function ranking()
+    {
+        return $this->select('results.racing_id', 'results.duration', 'racings.distance', 'results.participant_id', 'participants.birth', 'participants.name')
+                ->join('participants','participants.id', '=', 'results.participant_id')
+                ->join('racings','racings.id', '=', 'results.racing_id')
+                ->orderBy('racings.distance')
+                ->orderBy('results.duration')
+                ->get();
+    }
 }
